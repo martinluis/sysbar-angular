ee7ddf3ac9e856c847e6159be67d31d64c64117c
Luis Martinez <luisc.martinez@softtek.com>
2025-07-24 11:52:39 -0600

    Cashier - Fixing layout error

--- src/app/pages/cashier-page/cashier-page.ts
+++ src/app/pages/cashier-page/cashier-page.ts
@@ -3,9 +3,9 @@ import {HeaderComponent} from '../../components/header/header.component';
 import {OrderService} from '../../services/order.service';
 import {Observable} from 'rxjs';
 import {Order} from '../../models/order';
-import {AsyncPipe, DatePipe} from '@angular/common';
+import {AsyncPipe, CurrencyPipe, DatePipe} from '@angular/common';
 import {OrderType} from '../../models/order-type.enum';
-import {OrderSummaryComponent} from '../../components/order-summary/order-summary.component';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-cashier-page',
@@ -13,7 +13,7 @@ import {OrderSummaryComponent} from '../../components/order-summary/order-summar
     HeaderComponent,
     AsyncPipe,
     DatePipe,
-    OrderSummaryComponent
+    CurrencyPipe
   ],
   templateUrl: './cashier-page.html',
   styleUrl: './cashier-page.scss'
@@ -24,7 +24,7 @@ export class CashierPage implements OnInit{
   orders$!: Observable<Order[]>;
   orderSelected!: Order
 
-  constructor(private orderServices: OrderService) {
+  constructor(private orderServices: OrderService, private router: Router) {
   }
 
   /**
@@ -44,12 +44,21 @@ export class CashierPage implements OnInit{
 
   /**
    *
+   */
+  onPay() {
+    this.router.navigate(['pay'], {
+      queryParams: { orderId: this.orderSelected.id }
+    })
+  }
+
+  /**
+   *
    * @param order
    */
   getTypeDescription(order: Order): string {
       switch (order.orderType) {
         case OrderType.PERSONAL:
-          return "Personal";
+          return `Personal - ${order.reference}`;
         case OrderType.LOCAL:
           return order.table?.name ?? "Mesa";
         case OrderType.DELIVERY:
